a985322595f20f64cd42a7848936db5f963b54d2
Maureen-Gatweri <maureennmwendwa@gmail.com>
2024-03-07 07:04:16 +0300

    consolidate strings

---
+++ strings.js
@@ -0,0 +1,34 @@
+
+
+
+const str = `extravaganza`;
+const last4 = str.slice(-4);
+console.log(last4);
+
+
+
+
+
+
+const string1 = "The pupils are reading in the library";
+console.log(string1[11]+ "" +string1[12]+ "" +string1[13]);
+
+const string2 = "The child was sitting on the table before it fell";
+console.log(string2[12]+ ""+string2[15]+ ""+string2[16]+""+string2[17]+""+string2[18]+""+string2[19]+""+string2[20])
+
+
+ 
+
+
+
+let b = "wonderful";
+console.log(b.toUpperCase());
+
+
+let a = "Amazing";
+console.log(a.toLowerCase());
+
+let c = "undERneath";
+console.log(c.toLowerCase())
+
+
